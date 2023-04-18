2a03c21b3f055e24becebe3d902ba68356b8f3ba
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-04-18 17:14:35 +0700

    update 12

--- lib/screens/chi_tiet_chuong_page.dart
+++ lib/screens/chi_tiet_chuong_page.dart
@@ -426,6 +426,318 @@ class _ChiTietChuongPageState extends State<ChiTietChuongPage> {
     }
   ];
 
+  List<Map<String, dynamic>> listComment = [
+    {
+      "id": 49,
+      "idnguoidung": 1,
+      "idtruyen": 1,
+      "noidung": "Hay",
+      "ngaybinhluan": "2023-03-22T12:14:56.000Z",
+      "tennguoidung": "Nguyễn Văn Tèo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740"
+    },
+    {
+      "id": 47,
+      "idnguoidung": 10,
+      "idtruyen": 1,
+      "noidung": "Nice",
+      "ngaybinhluan": "2023-02-28T13:04:53.000Z",
+      "tennguoidung": "Lê Văn Duẫn",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1677589325446_2f9609ce-7e98-4779-98c8-329e417fdf8e.jpeg?alt=media&token=55e59b08-1a02-4dd0-b229-7646bc82441f"
+    },
+    {
+      "id": 46,
+      "idnguoidung": 9,
+      "idtruyen": 1,
+      "noidung": "Chào",
+      "ngaybinhluan": "2023-02-24T02:55:55.000Z",
+      "tennguoidung": "Meo meo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1677379216043_5cfd7bed-7375-476b-b69d-2d0fa3f40a0a.jpeg?alt=media&token=452e1e5c-59c3-43b4-9b79-b03a7105b809"
+    },
+    {
+      "id": 44,
+      "idnguoidung": 8,
+      "idtruyen": 1,
+      "noidung": "Hay đấy",
+      "ngaybinhluan": "2023-02-12T22:30:07.000Z",
+      "tennguoidung": "Hà Thu",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676240973274_d75a08dd-e8b8-4ef6-9dd4-5dcd44852361.jpeg?alt=media&token=1309928b-26e4-4f99-b2b1-95b82d22a872"
+    },
+    {
+      "id": 43,
+      "idnguoidung": 7,
+      "idtruyen": 1,
+      "noidung": "Xuất sắc",
+      "ngaybinhluan": "2023-02-12T22:20:01.000Z",
+      "tennguoidung": "Lý Thị Hoa",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676240265667_cca93e6c-621c-4ea8-822e-d0ae8b83d9f2.jpeg?alt=media&token=1b9be1a8-07d0-4ca8-8d72-9a6e718926ac"
+    },
+    {
+      "id": 42,
+      "idnguoidung": 3,
+      "idtruyen": 1,
+      "noidung": "Hay quá",
+      "ngaybinhluan": "2023-02-12T21:55:49.000Z",
+      "tennguoidung": "Trần Bá",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676591448513_aaeaa6ee-c579-4d62-a2d1-12960d717c87.jpeg?alt=media&token=89d0323f-e6f6-4c92-8705-d12e0e605df5"
+    },
+    {
+      "id": 40,
+      "idnguoidung": 1,
+      "idtruyen": 1,
+      "noidung": "Test bình luận lần 3",
+      "ngaybinhluan": "2023-02-10T22:31:32.000Z",
+      "tennguoidung": "Nguyễn Văn Tèo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740"
+    },
+    {
+      "id": 39,
+      "idnguoidung": 1,
+      "idtruyen": 1,
+      "noidung": "Test bình luận 2",
+      "ngaybinhluan": "2023-02-10T22:29:49.000Z",
+      "tennguoidung": "Nguyễn Văn Tèo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740"
+    },
+    {
+      "id": 38,
+      "idnguoidung": 1,
+      "idtruyen": 1,
+      "noidung": "Test bình luận 1",
+      "ngaybinhluan": "2023-02-10T22:28:38.000Z",
+      "tennguoidung": "Nguyễn Văn Tèo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740"
+    },
+    {
+      "id": 37,
+      "idnguoidung": 5,
+      "idtruyen": 1,
+      "noidung": "Tuyệt vời ông mặt trời :))",
+      "ngaybinhluan": "2023-02-10T21:26:44.000Z",
+      "tennguoidung": "User 5",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 36,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T21:26:44.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 35,
+      "idnguoidung": 3,
+      "idtruyen": 1,
+      "noidung": "Quá ghê gớm!!!!!!",
+      "ngaybinhluan": "2023-02-10T21:26:44.000Z",
+      "tennguoidung": "Trần Bá",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676591448513_aaeaa6ee-c579-4d62-a2d1-12960d717c87.jpeg?alt=media&token=89d0323f-e6f6-4c92-8705-d12e0e605df5"
+    },
+    {
+      "id": 34,
+      "idnguoidung": 2,
+      "idtruyen": 1,
+      "noidung": "Hay ghê á!!!!!!",
+      "ngaybinhluan": "2023-02-10T21:26:44.000Z",
+      "tennguoidung": "Nguyen Thi Hoa",
+      "avatar":
+          "https://genk.mediacdn.vn/zoom/700_438/2017/panda-1024-1488876248329-crop-1488876277745.jpg"
+    },
+    {
+      "id": 33,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T21:26:44.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 31,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T21:23:53.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 32,
+      "idnguoidung": 5,
+      "idtruyen": 1,
+      "noidung": "Tuyệt vời ông mặt trời :))",
+      "ngaybinhluan": "2023-02-10T21:23:53.000Z",
+      "tennguoidung": "User 5",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 30,
+      "idnguoidung": 3,
+      "idtruyen": 1,
+      "noidung": "Quá ghê gớm!!!!!!",
+      "ngaybinhluan": "2023-02-10T21:23:53.000Z",
+      "tennguoidung": "Trần Bá",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676591448513_aaeaa6ee-c579-4d62-a2d1-12960d717c87.jpeg?alt=media&token=89d0323f-e6f6-4c92-8705-d12e0e605df5"
+    },
+    {
+      "id": 29,
+      "idnguoidung": 2,
+      "idtruyen": 1,
+      "noidung": "Hay ghê á!!!!!!",
+      "ngaybinhluan": "2023-02-10T21:23:53.000Z",
+      "tennguoidung": "Nguyen Thi Hoa",
+      "avatar":
+          "https://genk.mediacdn.vn/zoom/700_438/2017/panda-1024-1488876248329-crop-1488876277745.jpg"
+    },
+    {
+      "id": 28,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T21:23:53.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 27,
+      "idnguoidung": 5,
+      "idtruyen": 1,
+      "noidung": "Tuyệt vời ông mặt trời :))",
+      "ngaybinhluan": "2023-02-10T21:23:33.000Z",
+      "tennguoidung": "User 5",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 26,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T21:23:33.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 25,
+      "idnguoidung": 3,
+      "idtruyen": 1,
+      "noidung": "Quá ghê gớm!!!!!!",
+      "ngaybinhluan": "2023-02-10T21:23:33.000Z",
+      "tennguoidung": "Trần Bá",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676591448513_aaeaa6ee-c579-4d62-a2d1-12960d717c87.jpeg?alt=media&token=89d0323f-e6f6-4c92-8705-d12e0e605df5"
+    },
+    {
+      "id": 24,
+      "idnguoidung": 2,
+      "idtruyen": 1,
+      "noidung": "Hay ghê á!!!!!!",
+      "ngaybinhluan": "2023-02-10T21:23:33.000Z",
+      "tennguoidung": "Nguyen Thi Hoa",
+      "avatar":
+          "https://genk.mediacdn.vn/zoom/700_438/2017/panda-1024-1488876248329-crop-1488876277745.jpg"
+    },
+    {
+      "id": 23,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T21:23:33.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 6,
+      "idnguoidung": 5,
+      "idtruyen": 1,
+      "noidung": "Tuyệt vời ông mặt trời :))",
+      "ngaybinhluan": "2023-02-10T19:52:24.000Z",
+      "tennguoidung": "User 5",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 5,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T19:52:24.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 4,
+      "idnguoidung": 3,
+      "idtruyen": 1,
+      "noidung": "Quá ghê gớm!!!!!!",
+      "ngaybinhluan": "2023-02-10T19:52:24.000Z",
+      "tennguoidung": "Trần Bá",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1676591448513_aaeaa6ee-c579-4d62-a2d1-12960d717c87.jpeg?alt=media&token=89d0323f-e6f6-4c92-8705-d12e0e605df5"
+    },
+    {
+      "id": 3,
+      "idnguoidung": 2,
+      "idtruyen": 1,
+      "noidung": "Hay ghê á!!!!!!",
+      "ngaybinhluan": "2023-02-10T19:52:24.000Z",
+      "tennguoidung": "Nguyen Thi Hoa",
+      "avatar":
+          "https://genk.mediacdn.vn/zoom/700_438/2017/panda-1024-1488876248329-crop-1488876277745.jpg"
+    },
+    {
+      "id": 2,
+      "idnguoidung": 4,
+      "idtruyen": 1,
+      "noidung": "Đỉnh thật sự",
+      "ngaybinhluan": "2023-02-10T19:52:24.000Z",
+      "tennguoidung": "User 4",
+      "avatar":
+          "https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/meo-anh-long-ngan-11.jpg"
+    },
+    {
+      "id": 1,
+      "idnguoidung": 1,
+      "idtruyen": 1,
+      "noidung": "Hay quá!!!!!!",
+      "ngaybinhluan": "2023-02-10T19:47:05.000Z",
+      "tennguoidung": "Nguyễn Văn Tèo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740"
+    },
+    {
+      "id": 22,
+      "idnguoidung": 1,
+      "idtruyen": 1,
+      "noidung": "Quá đỉnh :))",
+      "ngaybinhluan": "2022-12-01T05:00:00.000Z",
+      "tennguoidung": "Nguyễn Văn Tèo",
+      "avatar":
+          "https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740"
+    }
+  ];
   @override
   void initState() {
     super.initState();
@@ -640,7 +952,7 @@ class _ChiTietChuongPageState extends State<ChiTietChuongPage> {
                               ),
                               IconButton(
                                 onPressed: () {
-                                  showMySlidingSheet(context, listImage);
+                                  showMySlidingSheet(context, listComment);
                                 },
                                 splashColor: Colors.black,
                                 splashRadius: 22,
--- lib/widgets/comment_sliding_sheet_widget.dart
+++ lib/widgets/comment_sliding_sheet_widget.dart
@@ -1,3 +1,4 @@
+import 'package:comic_reading/common/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_sheet/sliding_sheet.dart';
 
@@ -10,23 +11,136 @@ void showMySlidingSheet(BuildContext context, List data) {
         snappings: [0.5, 0.9],
       ),
       builder: (context, state) => buildSheet(context, state, data),
+      footerBuilder: (context, state) => buildFooter(
+        context,
+        state,
+      ),
     ),
   );
 }
 
 Widget buildSheet(BuildContext context, SheetState state, List data) {
+  return Material(
+    child: ListView.builder(
+      itemCount: data.length,
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  data[index]['avatar'],
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            data[index]['tennguoidung'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '01/03/2023',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 4),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            data[index]['noidung'],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    ),
+  );
+}
+
+Widget buildFooter(
+  BuildContext context,
+  SheetState state,
+) {
   // sử dụng giá trị của data ở đây
   return Material(
     child: Container(
+      height: 60,
       padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-      child: Column(
-        children: [
-          Text(data.toString()), // hiển thị giá trị của data
-          Container(
-            height: 30,
-            color: Colors.red,
+      decoration: BoxDecoration(
+          // color: Colors.red,
           ),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  'https://firebasestorage.googleapis.com/v0/b/app-comic-reading.appspot.com/o/avatar%2F1678880613465_a0a963a1-2cb8-4a87-ba0f-b7ede566660c.jpeg?alt=media&token=5140dcb4-340a-4c5c-bbb4-f62154da4740',
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: Container(
+              // color: Colors.yellow,
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  hintText: 'Bình luận...',
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+          // SizedBox(width: 16),
+          IconButton(
+            onPressed: () {},
+            splashRadius: 22,
+            icon: Icon(Icons.send),
+            color: AppColors.ogrange,
+          )
         ],
       ),
     ),
