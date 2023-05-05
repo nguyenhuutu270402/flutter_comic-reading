de5c9c5b12a2b7fe88437c1575ba1af092ae2ad2
truong.thien <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-05-05 09:11:49 +0700

    update 33

--- lib/screens/danh_muc/danh_muc_page.dart
+++ lib/screens/danh_muc/danh_muc_page.dart
@@ -1,3 +1,6 @@
+import 'package:comic_reading/common/extension/custom_theme_extension.dart';
+import 'package:comic_reading/common/widgets/header_bar_widget.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +14,76 @@ class DanhMucPage extends StatefulWidget {
 class _DanhMucPageState extends State<DanhMucPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    final myColors = Theme.of(context).extension<CustomThemeExtension>()!;
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      color: Colors.black,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "Tài khoản",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.amber,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Image.network(
+                                "https://bizweb.dktcdn.net/100/303/962/files/87126502-2509242206005371-2073523065622364160-n-f697e400-e8b2-4bb1-9698-d00b50b2d9c3.jpg?v=1627804121650",
+                                width: 100,
+                                height: 100,
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Column(
+                                children: [
+                                  Text("Nguyen Van A"),
+                                  Text("Nguyen Van A"),
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            //header bar
+            HeaderBarWidget(
+              screenWidth: screenWidth,
+              myColors: myColors,
+              title: 'Trang chủ',
+              iconButtonRight: IconButton(
+                splashColor: Colors.black,
+                splashRadius: 22,
+                icon: Icon(Icons.search, color: myColors.blackOrWhite),
+                onPressed: () {
+                  print('Button timkiem pressed');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
